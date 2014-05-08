ba342ececfe92ab570b15b94a538bc0648de9990
WangSheng <sheng@consilium-asia.com>
2014-05-08 09:20:18 +0800

    2

--- webgl_test/webgl_playground.js
+++ webgl_test/webgl_playground.js
@@ -30,36 +30,6 @@ gl.attachShader(program, fShader);
 gl.linkProgram(program);
 gl.useProgram(program);
 
-//get the attribute/uniform indices in the shaders.
-var aPositionIdx = gl.getAttribLocation(program, "aPosition");
-var aNormalIdx = gl.getAttribLocation(program, "aNormal");
-var uMVPMatrixIdx = gl.getUniformLocation(program, "uMVPMatrix");
-var uNormalMatrixIdx = gl.getUniformLocation(program, "uNormalMatrix");
-
-gl.useProgram(program);
-
-//feed all kinds of matrices
-var viewMatrix = mat4.create();
-var projectionMatrix = mat4.create();
-var modelMatrix = mat4.create();
-var VPMatrix = mat4.create();
-var MVPMatrix = mat4.create();
-var MVMatrix = mat4.create();
-var normalMatrix = mat3.create();
-
-//MVP matrix...
-mat4.perspective(projectionMatrix, Math.PI * 0.1, gl.viewportWidth / gl.viewportHeight, 1, 2000.0);
-mat4.lookAt(viewMatrix, vec3.fromValues(10, 10, 40), vec3.fromValues(0,0,0), vec3.fromValues(0,1,0));
-mat4.multiply(VPMatrix, projectionMatrix, viewMatrix);
-mat4.multiply(MVPMatrix, VPMatrix, modelMatrix);
-gl.uniformMatrix4fv(uMVPMatrixIdx, false, MVPMatrix);
-
-
-//normal matrix..
-mat4.multiply(MVMatrix, viewMatrix, modelMatrix);
-mat3.normalFromMat4(normalMatrix, MVMatrix);
-gl.uniformMatrix3fv(uNormalMatrixIdx, false, normalMatrix);
-
 //feed the information of vertices.
 var vertices = new Float32Array([
     1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,
@@ -85,7 +55,8 @@ var indices = new Uint8Array([
     16,17,18,  16,18,19,
     20,21,22,  20,22,23 ]);
 
-//create buffers and feed them to the shaders
+//create buffers and feed them to the shaders, and always feed the vertices positions and vertices normals
+var aPositionIdx = gl.getAttribLocation(program, "MCVertex");
 var vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
@@ -93,6 +64,7 @@ gl.vertexAttribPointer(aPositionIdx, 3, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(aPositionIdx);
 gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
+var aNormalIdx = gl.getAttribLocation(program, "MCNormal");
 var normalBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
@@ -103,48 +75,25 @@ gl.bindBuffer(gl.ARRAY_BUFFER, null);
 var elementBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer);
 gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
 play();
 
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer);
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
+
+/**
+ * just before we draw elements... magic happens here.
+ */
 function play(){
-    //add diffuse lighting.
-    var uLightDirIdx = gl.getUniformLocation(program, "uLightDir");
-    var diffuseLightDirection = vec3.fromValues(10.0, 10.0, 40.0);
-    vec3.normalize(diffuseLightDirection, diffuseLightDirection);
-    gl.uniform3fv(uLightDirIdx, diffuseLightDirection);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
-
-
-
-
-    canvas.onclick = function(){
-        mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(1.5, 0, 2.5));
-        mat4.multiply(MVPMatrix, VPMatrix, modelMatrix);
-        gl.uniformMatrix4fv(uMVPMatrixIdx, false, MVPMatrix);
-        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
-
-        window.requestAnimationFrame(function(){
-            mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(1.5, 2, 2.5));
-            mat4.multiply(MVPMatrix, VPMatrix, modelMatrix);
-            gl.uniformMatrix4fv(uMVPMatrixIdx, false, MVPMatrix);
-            gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
-
-//            window.requestAnimationFrame(function(){
-//                mat4.translate(modelMatrix, modelMatrix, vec3.fromValues(1.5, 3, 2.5));
-//                mat4.multiply(MVPMatrix, VPMatrix, modelMatrix);
-//                gl.uniformMatrix4fv(uMVPMatrixIdx, false, MVPMatrix);
-//                gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
-//            });
-        });
-
-        console.log(1);
-
-
-    }
 
 }
+
+//function addDiffuseLight(){
+//    //add diffuse lighting.
+//    var uLightDirIdx = gl.getUniformLocation(program, "uLightDir");
+//    var diffuseLightDirection = vec3.fromValues(10.0, 10.0, 40.0);
+//    vec3.normalize(diffuseLightDirection, diffuseLightDirection);
+//    gl.uniform3fv(uLightDirIdx, diffuseLightDirection);
+//}
